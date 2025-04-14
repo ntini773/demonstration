e4f790ce21bec692ed18e9d2c1dc4726e5cff3bd
ntini773 <grandhi.sai@research.iiit.ac.in>
2025-04-14 22:23:01 +0530

    Add files via upload

---
+++ ibex/examples/sw/simple_system/hello_test/hello_test.d
@@ -0,0 +1,3 @@
+hello_test.o: hello_test.c \
+ /home/nitin/demonstration/ibex/examples/sw/simple_system/common/simple_system_common.h \
+ /home/nitin/demonstration/ibex/examples/sw/simple_system/common/simple_system_regs.h
